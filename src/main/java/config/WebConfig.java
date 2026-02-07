fe33a9d21654217072677331fd3c3b2fb6386f83
“floseptysia-hue” <floseptysia@gmail.com>
2026-02-07 23:22:42 +0700

    finsihing project

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -1,13 +1,14 @@
 package config;
 
-public class WebConfig {
+import io.github.cdimascio.dotenv.Dotenv;
 
+public class WebConfig {
+    private static final Dotenv dotenv = Dotenv.load();
     public static final String BASE_URL = "https://gist.github.com";
     public static final String BROWSER =
             System.getenv("BROWSER") != null ? System.getenv("BROWSER") : "chrome";
-
     public static final int TIMEOUT = 15;
-    public static String USERNAME = "floseptysia-hue";
-    public static String EMAIL_GITHUB = "";
-    public static String PASSWORD_GITHUB = "";
+    public static final String USERNAME = dotenv.get("USERNAME");
+    public static final String EMAIL_GITHUB = dotenv.get("EMAIL_GITHUB");
+    public static final String PASSWORD_GITHUB = dotenv.get("PASSWORD_GITHUB");
 }
